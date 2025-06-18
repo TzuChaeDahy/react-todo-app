17ed616bf122f78b016721935428ddb6bb775078
TzuChaeDahy <viniciuslovestwice@gmail.com>
2025-06-18 13:54:46 -0300

    implementando fetch da api e removendo strict mode

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -5,3 +5,47 @@ export const fetchTodos = async (url: string) => {
   }
   return response.json();
 };
+
+export const addTodo = async (url: string, todo: { title: string }) => {
+  const response = await fetch(`${url}/todos`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to add todo");
+  }
+
+  return response.json();
+};
+
+export const deleteTodo = async (url: string, id: string) => {
+  const response = await fetch(`${url}/todos/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to delete todo");
+  }
+};
+
+export const updateTodo = async (url: string, id: string) => {
+  const response = await fetch(`${url}/todos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to update todo");
+  }
+
+  return response.json();
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { ApiProvider } from "./contexts/ApiContext.tsx";
@@ -7,13 +6,11 @@ import { ThemeProvider } from "./contexts/ThemeContext.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ApiProvider>
-      <QueryClientProvider client={new QueryClient()}>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </QueryClientProvider>
-    </ApiProvider>
-  </StrictMode>
+  <ApiProvider>
+    <QueryClientProvider client={new QueryClient()}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </ApiProvider>
 );
